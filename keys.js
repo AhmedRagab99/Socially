679233d6f308fdf670f42eb51c316abd6d24c7d8
AhmedRagab <aragab22@gmail.com>
2020-06-28 07:37:24 +0200

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGOURI: "mongodb+srv://Ahmed:Ahmed@socially-app-jbdiw.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    JWTSECRET: "JWTSECRET"
+    MONGOURI:"",
+    JWTSECRET: ""
 }
